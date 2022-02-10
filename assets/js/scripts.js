d57ffca34699457dddc73e62fac3a6e26f2e517f
SalomeJaghmaidze <85635736+SalomeJaghmaidze@users.noreply.github.com>
2022-02-10 20:19:03 +0400

    adding error gif

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,64 +5,52 @@ function getUser() {
             return response.json();
         })
         .then(function (response) {
-            var name = response.name;
-            document.getElementById('name').innerText = name;
 
-            var userLogin = response.login;
-            document.getElementById('username').innerText = "@" + userLogin;
+            var main = document.getElementById('main');
+            var errorimage = document.getElementById('errorimg');
 
-            var image = response.avatar_url;
-            document.getElementById('mainImage').src = image;
+            if (response.message == "Not Found") {
+                main.style.display = "none";
+                errorimage.style.display = "block";
+                console.log('mushaobs')
+                return
+            }
+
+            errorimage.style.display = "none";
+            main.style.display = "flex";
+
+            document.getElementById('name').innerText = response.name;
+            document.getElementById('username').innerText = "@" + response.login;
+            document.getElementById('mainImage').src = response.avatar_url;
+            document.getElementById('reposNum').innerText = response.public_repos;
+            document.getElementById('followersNum').innerText = response.followers;
+            document.getElementById('followingNum').innerText = response.following;
 
             var userBio = response.bio;
-            if (userBio == null) {
-                document.getElementById('info-bio').innerText = "This profile has no bio";
-            } else {
-                document.getElementById('info-bio').innerText = userBio;
-            }
-
-            var reposNum = response.public_repos;
-            document.getElementById('reposNum').innerText = reposNum;
-
-            var followersNumber = response.followers;
-            document.getElementById('followersNum').innerText = followersNumber;
-
-            var followingNumber = response.following;
-            document.getElementById('followingNum').innerText = followingNumber;
-
+            document.getElementById('info-bio').innerText = userBio != null ? userBio : "This profile has no bio";;
 
             var location = response.location;
-            if (location == null) {
-                document.getElementById('location').innerText = "N/A";
-            } else {
-                document.getElementById('location').innerText = location;
-            }
+            document.getElementById('location').innerText = location != null ? location : "N/A";
 
-            var profile = response.html_url;
             var link = document.getElementById('profileUrl');
-            link.setAttribute('href', profile);
+            link.setAttribute('href', response.html_url);
             document.getElementById("profileUrl").innerText = "GitHub";
 
             var twitter = response.twitter_username;
-            if (twitter === null) {
-                document.getElementById("twitter-url").innerText = "Not Available";
-            } else {
-                var twitterUrl = document.getElementById('twitter-url');
-                twitterUrl.setAttribute('href', "https://twitter.com/" + twitter);
-                document.getElementById("twitter-url").innerText = twitter;
-            }
-
+            var twitterUrl = document.getElementById('twitter-url');
+            twitterUrl.setAttribute('href', "https://twitter.com/" + twitter);
+            document.getElementById("twitter-url").innerText = twitter != null ? twitter : "Not Available";
 
             var companyName = response.company;
-            if (companyName == null) {
-                document.getElementById('companyName').innerText = "N/A";
-            } else {
-                document.getElementById('companyName').innerText = companyName;
-            }
+            document.getElementById('companyName').innerText = companyName != null ? companyName : "N/A";
 
-            var joinedDate = response.created_at;
-            var d = new Date(joinedDate);
-            var date = d.toDateString();
-            document.getElementById('date').innerText = date;
+            var dateObject = new Date(response.created_at);
+            document.getElementById('date').innerText = dateObject.toDateString();;
+        })
+        .catch((error) => {
+            console.error('Error:', error);
         });
 }
+
+
+
